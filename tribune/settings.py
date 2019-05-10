e264b2b5cce70bc5f3dc71733d259ae8a72b8312
Jaccojwang <danolago@gmail.com>
2019-05-10 16:06:10 +0300

    Changes to deploy2

--- tribune/settings.py
+++ tribune/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import django-heroku
+import django_heroku
 from decouple import config,Csv
 
 MODE=config("MODE", default="dev")
@@ -170,4 +170,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django-heroku.settings(locals())
+django_heroku.settings(locals())
